84099f82e019cb1558a1e72864a499e39c59694f
andres <andres.sovik@tv2.no>
2023-12-25 21:32:56 +0100

    add enemy_move and player_move to game, also add more def to define

--- run.py
+++ run.py
@@ -67,6 +67,68 @@ board = create_board(GRID_SIZE)
 guess_board = create_guess_board(GRID_SIZE)
 place_ships(board, NUM_SHIPS, SHIP_SIZE)
 
+def player_move(guess_board):
+    while True:
+        try:
+            user_input = input("Enter your move (e.g., A3): ").upper()
+            row, col = ord(user_input[0]) - 65, int(user_input[1:]) - 1
+
+            if 0 <= row < GRID_SIZE and 0 <= col < GRID_SIZE:
+                if guess_board[row][col] == '~':
+                    return row, col
+                else:
+                    print("You've already guessed that spot. Try again.")
+            else:
+                print("Invalid move. Please enter a valid coordinate.")
+        except (ValueError, IndexError):
+            print("Invalid input. Please enter a valid coordinate.")
+
+
+def enemy_move(player_board):
+    while True:
+        row, col = random.randint(0, GRID_SIZE - 1), random.randint(0, GRID_SIZE - 1)
+        if player_board[row][col] == '.':
+            return row, col
+
+
+def update_board_after_move(board, guess_board, row, col):
+    # Add logic to update the board after a move
+
+def is_game_over(board):
+    # Add logic to check if the game is over (all ships have been hit)
+
+def main():
+    # Setup
+    board = create_board(GRID_SIZE)
+    guess_board = create_guess_board(GRID_SIZE)
+    place_ships(board, NUM_SHIPS, SHIP_SIZE)
+
+    while not is_game_over(board):
+        print("Your Board:")
+        print_board(board)
+
+        # Player's turn
+        player_row, player_col = player_move(guess_board)
+        player_guess(board, guess_board, player_row, player_col)
+        update_board_after_move(board, guess_board, player_row, player_col)
+
+        if is_game_over(board):
+            print("Game over! You win!")
+            break
+
+        # Enemy's turn
+        enemy_row, enemy_col = enemy_move(board)
+        # Update the board with the enemy's move and provide feedback
+
+        if is_game_over(board):
+            print("Game over! You lose.")
+            break
+
+if __name__ == "__main__":
+    display_game_rules()
+    main()
+
+
 # Testing the setup with a sample guess (replace with actual gameplay logic later)
 print("Player's Board View:")
 print_board(board)
@@ -76,6 +138,9 @@ player_guess(board, guess_board, 2, 3)  # Replace with player input
 
 print("\nPlayer's Guess Board:")
 print_board(guess_board)
+
+
+
 
 # Game rules display
 def display_game_rules():
