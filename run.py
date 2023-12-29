1a491df2bb8f639c8398e02d007c7523e9903bb1
andres <andres.sovik@tv2.no>
2023-12-29 17:27:19 +0100

    fix logic on how the ships were set on the board

--- run.py
+++ run.py
@@ -70,18 +70,18 @@ def place_ships(board, num_ships, ship_size):
 
 # Check if a ship can be placed at the specified location
 def can_place_ship(board, row, col, ship_size, orientation):
-    """Check if a ship can be placed at the specified location."""
+    """Check if a ship can be placed at the specified location with spacing."""
     if orientation == 'horizontal':
-        if col + ship_size > GRID_SIZE:
+        if col + ship_size > GRID_SIZE or (col > 0 and board[row][col - 1] == SHIP_SYMBOL) or (col + ship_size < GRID_SIZE and board[row][col + ship_size] == SHIP_SYMBOL):
             return False
-        for c in range(col, col + ship_size):
-            if board[row][c] != '.':
+        for c in range(max(0, col - 1), min(GRID_SIZE, col + ship_size + 1)):
+            if any(board[r][c] == SHIP_SYMBOL for r in range(max(0, row - 1), min(GRID_SIZE, row + 2))):
                 return False
     else:  # vertical
-        if row + ship_size > GRID_SIZE:
+        if row + ship_size > GRID_SIZE or (row > 0 and board[row - 1][col] == SHIP_SYMBOL) or (row + ship_size < GRID_SIZE and board[row + ship_size][col] == SHIP_SYMBOL):
             return False
-        for r in range(row, row + ship_size):
-            if board[r][col] != '.':
+        for r in range(max(0, row - 1), min(GRID_SIZE, row + ship_size + 1)):
+            if any(board[r][c] == SHIP_SYMBOL for c in range(max(0, col - 1), min(GRID_SIZE, col + 2))):
                 return False
     return True
 
@@ -154,12 +154,14 @@ def main():
 
     display_game_rules()
 
-    board = create_board(GRID_SIZE)
-    guess_board = create_guess_board(GRID_SIZE)
-    place_ships(board, NUM_SHIPS, SHIP_SIZE)
+    player_board = create_board(GRID_SIZE)  # Player's board
+    enemy_board = create_board(GRID_SIZE)  # Enemy's board
+    guess_board = create_guess_board(GRID_SIZE)  # Player's guess board
 
-    enemy_previous_moves = set()  # Initialize the set to track enemy's previous moves
+    place_ships(player_board, NUM_SHIPS, SHIP_SIZE)  # Place player's ships
+    place_ships(enemy_board, NUM_SHIPS, SHIP_SIZE)  # Place enemy's ships
 
+    enemy_previous_moves = set()  # Track enemy's previous moves
 
     while True:
         print(f"\n{player_name}'s Guesses:")
