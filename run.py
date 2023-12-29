8b1dda7deba6580b9e77657c09cce63e298915ee
andres <andres.sovik@tv2.no>
2023-12-29 22:57:37 +0100

    add new enemy move logic so it does not repeated previous moves.

--- run.py
+++ run.py
@@ -127,13 +127,15 @@ def player_move(guess_board):
             print("Invalid input. Please enter a valid coordinate.")
 
 
-def enemy_move(player_board):
-    """Randomly generate a move for the enemy."""
+def enemy_move(player_board, previous_moves):
+    """Generate a smarter move for the enemy. Avoid repeating previous moves."""
     while True:
         row, col = random.randint(0, GRID_SIZE - 1), random.randint(0, GRID_SIZE - 1)
-        if player_board[row][col] == '.':
+        if (row, col) not in previous_moves:
+            previous_moves.add((row, col))
             return row, col
-        
+
+
 def enemy_move(player_board, previous_moves):
     """Generate a smarter move for the enemy. Avoid repeating previous moves."""
     while True:
