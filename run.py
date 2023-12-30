c3bb0b80113618113914954f888603e3400e6e4d
andres <andres.sovik@tv2.no>
2023-12-30 22:33:22 +0100

    Repeated Code for Checking Ship Placement and Optimizing Board Printing

--- run.py
+++ run.py
@@ -37,22 +37,22 @@ def create_board(size):
 
 # Function to display the board
 def print_board(board, board_type, hide_ships=False):
-    # Choose color based on board type
+    """Prints the game board with color-coded symbols."""
+        # Choose color based on board type
     board_color = Fore.GREEN if board_type == 'player' else Fore.YELLOW
+    header = "  " + " ".join(str(i + 1) for i in range(GRID_SIZE))
+    print(board_color + header)
 
-    print(board_color + "  " + " ".join(str(i + 1) for i in range(GRID_SIZE)))
-    for i, row in enumerate(board):
-        colored_row = []
-        for cell in row:
-            if cell == HIT_SYMBOL:
-                colored_row.append(Fore.RED + HIT_SYMBOL)
-            elif cell == MISS_SYMBOL:
-                colored_row.append(Fore.BLUE + MISS_SYMBOL)
-            elif cell == SHIP_SYMBOL and not hide_ships:
-                colored_row.append(board_color + SHIP_SYMBOL)
-            else:
-                colored_row.append(board_color + cell)
-        print(board_color + chr(65 + i) + " " + " ".join(colored_row))
+    for row_index, row in enumerate(board):
+        colored_row = [
+            Fore.RED + HIT_SYMBOL if cell == HIT_SYMBOL else
+            Fore.BLUE + MISS_SYMBOL if cell == MISS_SYMBOL else
+            board_color + SHIP_SYMBOL if cell == SHIP_SYMBOL and not hide_ships else
+            board_color + cell
+            for cell in row
+        ]
+        print(board_color + chr(65 + row_index) + " " + " ".join(colored_row))
+
 
 
 # Initialize the guess board
@@ -94,24 +94,46 @@ def place_ships(board, num_ships, ship_size):
             set_ship(board, row, col, ship_size, orientation)
             ships_placed += 1
 
+def is_valid_placement(board, start_row, start_col, ship_size, orientation):
+    """
+    Check if a ship can be placed at the specified location without overlapping other ships.
+    """
+    if orientation == 'horizontal':
+        if start_col + ship_size > GRID_SIZE:
+            return False
+        for c in range(start_col, start_col + ship_size):
+            if board[start_row][c] == SHIP_SYMBOL:
+                return False
+    else:  # vertical
+        if start_row + ship_size > GRID_SIZE:
+            return False
+        for r in range(start_row, start_row + ship_size):
+            if board[r][start_col] == SHIP_SYMBOL:
+                return False
+    
+    return True
+
+
 
 # Check if a ship can be placed at the specified location
 def can_place_ship(board, row, col, ship_size, orientation):
-    """Check if a ship can be placed at the specified location with spacing."""
-    # Horizontal placement checks
+    """ 
+    Check if a ship can be placed at the specified location with proper spacing.
+    """
+    # Ensure the ship doesn't overlap with another ship and is within grid boundaries
+    if not is_valid_placement(board, row, col, ship_size, orientation):
+        return False
+
+    # Check for spacing around the ship
     if orientation == 'horizontal':
-        if col + ship_size > GRID_SIZE or (col > 0 and board[row][col - 1] == SHIP_SYMBOL) or (col + ship_size < GRID_SIZE and board[row][col + ship_size] == SHIP_SYMBOL):
-            return False
         for c in range(max(0, col - 1), min(GRID_SIZE, col + ship_size + 1)):
             if any(board[r][c] == SHIP_SYMBOL for r in range(max(0, row - 1), min(GRID_SIZE, row + 2))):
                 return False
-    # Vertical placement checks
-    else:
-        if row + ship_size > GRID_SIZE or (row > 0 and board[row - 1][col] == SHIP_SYMBOL) or (row + ship_size < GRID_SIZE and board[row + ship_size][col] == SHIP_SYMBOL):
-            return False
+    else:  # vertical
         for r in range(max(0, row - 1), min(GRID_SIZE, row + ship_size + 1)):
             if any(board[r][c] == SHIP_SYMBOL for c in range(max(0, col - 1), min(GRID_SIZE, col + 2))):
                 return False
+
     return True
 
 
@@ -152,17 +174,19 @@ def player_move(guess_board):
 
 def enemy_move(player_board, previous_moves, last_hit):
     """Generate a move for the enemy with a strategy after a hit."""
-    if last_hit:
-        # Generate strategic moves around the last hit
-        for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+    if last_hit and isinstance(last_hit, tuple) and len(last_hit) == 2 and all(isinstance(coord, int) for coord in last_hit):
+        # Check adjacent cells in a specific order
+        for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:  # Up, down, left, right
             new_row, new_col = last_hit[0] + dx, last_hit[1] + dy
             if 0 <= new_row < GRID_SIZE and 0 <= new_col < GRID_SIZE:
-                if (new_row, new_col) not in previous_moves:
+                if (new_row, new_col) not in previous_moves and player_board[new_row][new_col] != MISS_SYMBOL:
                     previous_moves.add((new_row, new_col))
                     return make_enemy_guess(player_board, new_row, new_col)
-    
-    # Random move if no hit to follow up
+
+    # Random move if no hit to follow up or all adjacent cells have been guessed
     return random_enemy_guess(player_board, previous_moves)
+
+
 
 def make_enemy_guess(player_board, row, col):
     """Process the enemy's guess."""
@@ -249,6 +273,7 @@ def main():
     player_wins = 0  # Counter for player's wins
     player_losses = 0  # Counter for player's losses
     
+   
     while True:
         print(f"Welcome to Battleship, {player_name}!")
 
@@ -277,7 +302,7 @@ def main():
                 print_board(guess_board, 'enemy', hide_ships=True)
 
                 
-
+        
             player_row, player_col = player_move(guess_board)
             player_result = player_guess(enemy_board, guess_board, player_row, player_col)
             last_move_summary = f"Last Move: {player_result}"
@@ -291,9 +316,10 @@ def main():
             enemy_result, hit_coords = enemy_move(player_board, enemy_previous_moves, last_hit)
             last_move_summary += f" | {enemy_result}"
             if hit_coords:
-                last_hit = hit_coords
+                last_hit = hit_coords  # Update last_hit if there's a new hit
             else:
-                last_hit = None  # Reset last hit if the enemy misses
+                last_hit = None  # Reset last_hit if the enemy misses
+
 
             if is_game_over(player_board, guess_board, NUM_SHIPS, SHIP_SIZE):
                 print(Fore.RED + f"Sorry, {player_name}, you lost the game.")
@@ -317,6 +343,3 @@ player_guess(board, guess_board, 2, 3)  # Replace with player input
 
 print("\nPlayer's Guess Board:")
 print_board(guess_board)
-
-
-
